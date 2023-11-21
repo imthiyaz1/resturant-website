43d720cc5058439afd42367f828473f94ed4d6c9
imthiyaz1 <imthiyazshaik859@gmail.com>
2023-11-21 10:52:53 +0530

    first deployment

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import Dosa from "../images/dosa.jpg";
+import Chhola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.png";
+import Paneer from "../images/panner.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chhola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+  
+];
